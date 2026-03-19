8b292edc11cac1e9be6027cdef68ac9be36c5667
Bartek <xbartoszdobija@gmail.com>
2026-03-19 21:55:25 +0100

    test

--- src/app/users-generator/template.tsx
+++ src/app/users-generator/template.tsx
@@ -72,7 +72,6 @@ export default function UsersGeneratorTemplate({ children }: { children: ReactNo
           </div>
         </div>
       </Sidebar>
-
       <div className={cn("ml-10", "px-6")}>
         {children}
       </div>
