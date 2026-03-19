bc90504d57b5700d23d1d80ae15274c4eec623be
Bartek <xbartoszdobija@gmail.com>
2026-03-19 21:38:47 +0100

    Add name and surname toggles to UsersGeneratorTemplate and update context for user settings

--- src/app/users-generator/page.tsx
+++ src/app/users-generator/page.tsx
@@ -3,11 +3,20 @@ import { UsersGeneratorContext } from "./template";
 import { useContext } from "react";
 
 export default function UsersGeneratorPage() {
-  const { username: isUsername, avatar: isAvatar, email: isEmail, password: isPassword } = useContext(UsersGeneratorContext);
+  const {
+    name: isName,
+    surname: isSurname,
+    username: isUsername,
+    avatar: isAvatar,
+    email: isEmail,
+    password: isPassword
+  } = useContext(UsersGeneratorContext);
 
   return (
     <>
       <h1>Random users generator</h1>
+      <p>Name: {isName ? "enabled" : "disabled"}</p>
+      <p>Surname: {isSurname ? "enabled" : "disabled"}</p>
       <p>Username: {isUsername ? "enabled" : "disabled"}</p>
       <p>Avatar: {isAvatar ? "enabled" : "disabled"}</p>
       <p>Email: {isEmail ? "enabled" : "disabled"}</p>
--- src/app/users-generator/template.tsx
+++ src/app/users-generator/template.tsx
@@ -6,6 +6,8 @@ import Button from "@/components/ui/button";
 import { cn } from "@/utilities";
 
 type GeneratorSettings = {
+  name: boolean;
+  surname: boolean;
   username: boolean;
   avatar: boolean;
   email: boolean;
@@ -13,6 +15,8 @@ type GeneratorSettings = {
 }
 
 export const UsersGeneratorContext = createContext<GeneratorSettings>({
+  name: false,
+  surname: false,
   username: false,
   avatar: false,
   email: false,
@@ -20,16 +24,28 @@ export const UsersGeneratorContext = createContext<GeneratorSettings>({
 });
 
 export default function UsersGeneratorTemplate({ children }: { children: ReactNode }) {
+  const [name, setName] = useState(false);
+  const [surname, setSurname] = useState(false);
   const [username, setUsername] = useState(false);
   const [avatar, setAvatar] = useState(false);
   const [email, setEmail] = useState(false);
   const [password, setPassword] = useState(false);
 
   return (
-    <UsersGeneratorContext.Provider value={{ username, avatar, email, password }}>
+    <UsersGeneratorContext.Provider value={{ name, surname, username, avatar, email, password }}>
       <Sidebar>
         <h2 className="mb-4">Settings</h2>
         <div className={cn("grid-2-columns", "width-100", "gap-2")}>
+
+          <div>Name</div>
+          <div className={cn("justify-self-end", "flex-align-center")}>
+            <Toggle onChange={(e) => setName(e.target.checked)} />
+          </div>
+
+          <div>Surname</div>
+          <div className={cn("justify-self-end", "flex-align-center")}>
+            <Toggle onChange={(e) => setSurname(e.target.checked)} />
+          </div>
 
           <div>Username</div>
           <div className={cn("justify-self-end", "flex-align-center")}>
