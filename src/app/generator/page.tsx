b1351990f457ecfdce9affffef4d571c68e5a8fa
Bartek <xbartoszdobija@gmail.com>
2026-04-13 01:11:27 +0200

    feat: integrate CodeGroup component for enhanced code display in UsersGeneratorPage

--- src/app/generator/page.tsx
+++ src/app/generator/page.tsx
@@ -11,6 +11,7 @@ import Icon from "@/components/ui/icon";
 import { cn } from "@/utilities";
 import { Column } from "@/types/generator";
 import { useGenerateData } from "@/hooks/useGenerateData";
+import { CodeGroup } from "@prose-ui/next";
 
 export default function UsersGeneratorPage() {
   const { columns, count, seed, isSeedEnabled } = useContext(UsersGeneratorContext);
@@ -68,11 +69,17 @@ export default function UsersGeneratorPage() {
   const [isCopied, setIsCopied] = useState<boolean>(false);
 
   const highlightedJson = useMemo(() => {
-    return Prism.highlight(jsonText, Prism.languages.json, "json");
+    const test = Prism.highlight(jsonText, Prism.languages.json, "json");
+    return `<pre><code>${test.split('\n').reduce((acc, line) => {
+      return acc + `<span class="line">${line}</span>`;
+    }, '')}</code></pre>`;
   }, [jsonText]);
 
   const highlightedSql = useMemo(() => {
-    return Prism.highlight(sqlText, Prism.languages.sql, "sql");
+    const test = Prism.highlight(sqlText, Prism.languages.sql, "sql");
+    return `<pre><code>${test.split('\n').reduce((acc, line) => {
+      return acc + `<span class="line">${line}</span>`;
+    }, '')}</code></pre>`;
   }, [sqlText]);
 
   const copyJson = async () => {
@@ -100,34 +107,21 @@ export default function UsersGeneratorPage() {
       <h1>Random users generator</h1>
       <p>You can choose what gets generated in the settings.</p>
       <h3 className={cn("mt-4")}>{isLoading ? "Loading..." : "Up to date!"}</h3>
-
-      <SwitchSection className="mt-2">
-        <div id="json">
-          <div className="code">
-            <Button onClick={copyJson} className={cn("copy", { "display-none": highlightedJson === "" })}>
-              <Icon name="copy" className="size-xs" />
-              Copy
-              <div className={cn("feedback", { "display-none": !isCopied })}>Copied to clipboard</div>
-            </Button>
-            <pre>
-              <code className="language-json" dangerouslySetInnerHTML={{ __html: highlightedJson }} />
-            </pre>
-          </div>
-        </div>
-
-        <div id="sql">
-          <div className="code">
-            <Button onClick={copySql} className={cn("copy", { "display-none": highlightedSql === "" })}>
-              <Icon name="copy" className="size-xs" />
-              Copy
-              <div className={cn("feedback", { "display-none": !isCopied })}>Copied to clipboard</div>
-            </Button>
-            <pre>
-              <code className="language-sql" dangerouslySetInnerHTML={{ __html: highlightedSql }} />
-            </pre>
-          </div>
-        </div>
-      </SwitchSection>
+      <div className="prose-ui">
+        <CodeGroup tabs={[{
+          title: "Code",
+          variants: {
+            json: {
+              code: jsonText,
+              highlightedCode: highlightedJson,
+            },
+            sql: {
+              code: sqlText,
+              highlightedCode: highlightedSql,
+            },
+          },
+        }]} languages={[{ value: "json", label: "JSON" }, { value: "sql", label: "SQL" }]} />
+      </div>
     </>
   );
 }
